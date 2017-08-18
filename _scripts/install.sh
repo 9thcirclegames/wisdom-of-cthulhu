8ad8cd943dbdd72d8c689510cf720a8968449618
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 13:38:35 +0200

    fix in export env

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -4,5 +4,7 @@ set -x
 wget https://github.com/lifelike/countersheetsextension/archive/2.0.zip -P /tmp/countersheetsextension/
 unzip /tmp/countersheetsextension/2.0.zip -d $TRAVIS_BUILD_DIR/
 
+export PYTHONPATH /usr/local/lib/python:/usr/share/inkscape/extensions/:$PYTHONPATH
+
 
 
