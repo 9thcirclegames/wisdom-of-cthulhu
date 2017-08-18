ea8251630344b32a610e967fa1a5a502e0687b0f
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 22:38:10 +0200

    set registration marks

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -2,4 +2,4 @@
 set -x
 
 mkdir $TRAVIS_BUILD_DIR/build
-python $TRAVIS_BUILD_DIR/countersheet.py -d $TRAVIS_BUILD_DIR/english.cards.csv $TRAVIS_BUILD_DIR/english.cards.svg > test.svg
+python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv $TRAVIS_BUILD_DIR/english.cards.svg > standard.deck.svg
