630ed35bfa4e15eaf00da3e7016e87d7b056011a
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 22:44:28 +0200

    add optional parameters

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -2,4 +2,5 @@
 set -x
 
 mkdir $TRAVIS_BUILD_DIR/build
-python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv $TRAVIS_BUILD_DIR/english.cards.svg > standard.deck.svg
+mkdir $TRAVIS_BUILD_DIR/pdf
+python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/pdf $TRAVIS_BUILD_DIR/english.cards.svg > $TRAVIS_BUILD_DIR/build/standard.deck.svg
--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 set -x
+
+ls $TRAVIS_BUILD_DIR/build
+ls $TRAVIS_BUILD_DIR/pdf
