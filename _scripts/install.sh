62c592312f469eae850b3be87f683d7e0c1e1f0b
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 13:43:49 +0200

    unzip only the needed files

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,9 +2,7 @@
 set -x
 
 wget https://github.com/lifelike/countersheetsextension/archive/2.0.zip -P /tmp/countersheetsextension/
-unzip /tmp/countersheetsextension/2.0.zip -d $TRAVIS_BUILD_DIR/
-
-export PYTHONPATH /usr/local/lib/python:/usr/share/inkscape/extensions/:$PYTHONPATH
+unzip -j "/tmp/countersheetsextension/2.0.zip" "countersheet.py" -d $TRAVIS_BUILD_DIR/
 
 
 
