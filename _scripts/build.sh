799a7e9230fbbf4d323f933ac3c097ea322a8408
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 13:35:20 +0200

    switch to python 2.7

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 set -x
+
+mkdir $TRAVIS_BUILD_DIR/build
+python $TRAVIS_BUILD_DIR/countersheet.py -d $TRAVIS_BUILD_DIR/english.cards.csv $TRAVIS_BUILD_DIR/english.cards.svg
--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,6 +2,7 @@
 set -x
 
 wget https://github.com/lifelike/countersheetsextension/archive/2.0.zip -P /tmp/countersheetsextension/
-#unzip /tmp/countersheetsextension/2.0.zip -d ~/.config/inkscape/extensions/
-ls ~/.config/
+unzip /tmp/countersheetsextension/2.0.zip -d $TRAVIS_BUILD_DIR/
+
+
 
