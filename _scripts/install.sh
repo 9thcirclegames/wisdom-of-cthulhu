2646e2f74affcba964d966add86fdebbabd053cd
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 13:04:17 +0200

    switch to python dock

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -x
 
-sudo apt-get update -q
-sudo apt-get install wget python3.6 inkscape libxml2-dev libxslt-dev python-dev zlib1g-dev pkg-config python-pip
-
-easy_install lxml
-
 wget https://github.com/lifelike/countersheetsextension/archive/2.0.zip -P /tmp/countersheetsextension/
-unzip /tmp/countersheetsextension/2.0.zip -d ~/.config/inkscape/extensions/
+#unzip /tmp/countersheetsextension/2.0.zip -d ~/.config/inkscape/extensions/
+ls ~/.config/
 
