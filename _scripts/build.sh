c7ef667fbd6e153f5f554c1eeafd38ee0ce908d4
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 23:48:13 +0200

    fix in pdf merging

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -10,6 +10,3 @@ mkdir $TRAVIS_BUILD_DIR/pdf.it
 python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/pdf.en $TRAVIS_BUILD_DIR/english.cards.svg > $TRAVIS_BUILD_DIR/build.en/standard.deck.svg
 
 pdfunite *.pdf standard.en.pdf
-
-shopt -s extglob 
-$ rm -- !(standard.en.pdf)
