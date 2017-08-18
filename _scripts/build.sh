06de3b7fc59bace8ac2f080f5077a13e60f32779
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 23:52:43 +0200

    switch to pdftk

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -9,4 +9,4 @@ mkdir $TRAVIS_BUILD_DIR/pdf.it
 
 python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/pdf.en $TRAVIS_BUILD_DIR/english.cards.svg > $TRAVIS_BUILD_DIR/build.en/standard.deck.svg
 
-pdfunite *.pdf standard.en.pdf
+pdftk $TRAVIS_BUILD_DIR/pdf.en/*.pdf cat output $TRAVIS_BUILD_DIR/pdf.en/standard.en.pdf
