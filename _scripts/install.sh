b7d4dd51a5efe7b362fdd468b79305e4cb0f6cbd
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 22:19:40 +0200

    fix in inkscape download

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,7 @@
 set -x
 
 wget https://github.com/lifelike/countersheetsextension/archive/2.0.tar.gz -P /tmp/countersheetsextension/
-tar -xzf /tmp/countersheetsextension/2.0.tar.gz --strip-components 1 countersheetsextension-2.0/countersheet.py -C /target/directory $TRAVIS_BUILD_DIR/
+tar -xzf /tmp/countersheetsextension/2.0.tar.gz --strip-components 1 countersheetsextension-2.0/countersheet.py -C $TRAVIS_BUILD_DIR/
 
 
 
