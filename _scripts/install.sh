6a4c1b9219867675c849962507689b89ea7abc70
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-18 22:13:30 +0200

    switch to tar

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 
-wget https://github.com/lifelike/countersheetsextension/archive/2.0.zip -P /tmp/countersheetsextension/
-unzip -j "/tmp/countersheetsextension/2.0.zip" "countersheet.py" -d $TRAVIS_BUILD_DIR/
+wget https://github.com/lifelike/countersheetsextension/archive/2.0.tar.gz -P /tmp/countersheetsextension/
+tar -xzf /tmp/countersheetsextension/2.0.tar.gz --strip-components 1 countersheetsextension-2.0/countersheet.py -C /target/directory $TRAVIS_BUILD_DIR/
 
 
 
