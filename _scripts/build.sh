048fec3dd0d7f87662b060be76de8ebbc606cde0
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-19 13:59:29 +0200

    fix in output folders

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -4,7 +4,7 @@ set -x
 mkdir $TRAVIS_BUILD_DIR/build
 mkdir $TRAVIS_BUILD_DIR/pdf
 
-python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/build $TRAVIS_BUILD_DIR/english.cards.svg > $TRAVIS_BUILD_DIR/build.en/standard.deck.svg
+python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/build $TRAVIS_BUILD_DIR/english.cards.svg > $TRAVIS_BUILD_DIR/build/standard.deck.svg
 
 pdftk $TRAVIS_BUILD_DIR/build/*.pdf cat output $TRAVIS_BUILD_DIR/pdf/standard.en.pdf
 
--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -x
 
-ls $TRAVIS_BUILD_DIR/build.en
-ls $TRAVIS_BUILD_DIR/pdf.en
-
-ls $TRAVIS_BUILD_DIR/build.it
-ls $TRAVIS_BUILD_DIR/pdf.it
+ls $TRAVIS_BUILD_DIR/build
+ls $TRAVIS_BUILD_DIR/pdf
