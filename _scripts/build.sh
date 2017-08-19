65367fb6b2328041ffcb227867fc343c5e2cb505
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-19 15:01:02 +0200

    add italian standard deck

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -4,8 +4,10 @@ set -x
 mkdir $TRAVIS_BUILD_DIR/build
 mkdir $TRAVIS_BUILD_DIR/pdf
 
-python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/build $TRAVIS_BUILD_DIR/english.cards.svg > $TRAVIS_BUILD_DIR/build/standard.deck.svg
+python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/build $TRAVIS_BUILD_DIR/english.cards.svg > $TRAVIS_BUILD_DIR/build/en.standard.deck.svg
+pdftk $TRAVIS_BUILD_DIR/build/*.pdf cat output $TRAVIS_BUILD_DIR/pdf/standard.deck.en.pdf
+rm $TRAVIS_BUILD_DIR/build/*.pdf
 
-pdftk $TRAVIS_BUILD_DIR/build/*.pdf cat output $TRAVIS_BUILD_DIR/pdf/standard.en.pdf
-
+python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/build $TRAVIS_BUILD_DIR/italian.cards.svg > $TRAVIS_BUILD_DIR/build/it.standard.deck.svg
+pdftk $TRAVIS_BUILD_DIR/build/*.pdf cat output $TRAVIS_BUILD_DIR/pdf/standard.deck.it.pdf
 rm $TRAVIS_BUILD_DIR/build/*.pdf
--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -x
 
-ls $TRAVIS_BUILD_DIR/build
 ls $TRAVIS_BUILD_DIR/pdf
