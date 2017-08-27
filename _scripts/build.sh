34cf84617cf7e0944227dd1ea673103a4f5aab3d
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2017-08-27 22:25:18 +0200

    added file versioning

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -8,11 +8,14 @@ mkdir $TRAVIS_BUILD_DIR/pdf
 
 python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/build $TRAVIS_BUILD_DIR/english.cards.svg > $TRAVIS_BUILD_DIR/build/en.standard.deck.svg
 pdftk $TRAVIS_BUILD_DIR/build/*.pdf cat output $TRAVIS_BUILD_DIR/pdf/woc.deck.en.pdf
+cp $TRAVIS_BUILD_DIR/pdf/woc.deck.en.pdf $TRAVIS_BUILD_DIR/pdf/woc.deck.en-$(date '+%Y%m%d').pdf
 rm $TRAVIS_BUILD_DIR/build/*.pdf
 
 python $TRAVIS_BUILD_DIR/countersheet.py -r 30 -n deck -d $TRAVIS_BUILD_DIR/english.cards.csv -p $TRAVIS_BUILD_DIR/build $TRAVIS_BUILD_DIR/italian.cards.svg > $TRAVIS_BUILD_DIR/build/it.standard.deck.svg
 pdftk $TRAVIS_BUILD_DIR/build/*.pdf cat output $TRAVIS_BUILD_DIR/pdf/woc.deck.it.pdf
+cp $TRAVIS_BUILD_DIR/pdf/woc.deck.it.pdf $TRAVIS_BUILD_DIR/pdf/woc.deck.it-$(date '+%Y%m%d').pdf
 rm $TRAVIS_BUILD_DIR/build/*.pdf
 
 # Build rules PDF
 pandoc --variable urlcolor=cyan $TRAVIS_BUILD_DIR/woc.rules.en.md --variable mainfont="Liberation Serif" --variable sansfont="Liberation Sans" -o $TRAVIS_BUILD_DIR/pdf/woc.rules.en.pdf
+cp $TRAVIS_BUILD_DIR/pdf/woc.rules.en.pdf $TRAVIS_BUILD_DIR/pdf/woc.rules.en-$(date '+%Y%m%d').pdf
---
+++ docs/index.md
@@ -0,0 +1,17 @@
+---
+title: Wisdom of Cthulhu
+description: Nighlty Builds - Print and Play
+---
+
+## Wisdom of Cthulhu
+
+### Nightly Builds
+
+* __English__
+	* [Game Rules](https://static.9thcircle.it/woc-builds/woc.rules.en.pdf)
+	* [Cards Deck](https://static.9thcircle.it/woc-builds/woc.deck.en.pdf)
+* __Italian__
+	* [Cards Deck](https://static.9thcircle.it/woc-builds/woc.deck.it.pdf)
+
+### Old Releases
+
